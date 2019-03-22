0427b5f3230d65305a14bdb69b62904f02421052
Takayuki Nagata <tnagata@redhat.com>
2019-03-22 14:20:29 +0900

    Rename *.c files for each test cases to reflect the latest README.

---
+++ SLUB/TEST-0/CASE-3.c
@@ -0,0 +1,18 @@
+/* For testing memory corruption.
+ * Copyright (c) 2019 Takayuki Nagata
+ *
+ * TEST 0-3: monitor right behavior in allocating the 2nd object
+ */
+
+#include <linux/slab.h>
+#include "corrupter.h"
+#include "corrupter_TEST.h"
+
+void corrupter_slab_doit(struct kmem_cache *cachep)
+{
+	struct corrupter_obj *obj0, *obj1, *obj2;
+	obj0 = obj1 = obj2 = NULL;
+
+	STEP_02
+	STEP_03
+}
---
+++ SLUB/TEST-B/CASE-3a.c
@@ -0,0 +1,19 @@
+/* For testing memory corruption.
+ * Copyright (c) 2019 Takayuki Nagata
+ *
+ * TEST B-3a: use corrupted freelist with allocating new object
+ */
+
+#include <linux/slab.h>
+#include "corrupter.h"
+#include "corrupter_TEST.h"
+
+void corrupter_slab_doit(struct kmem_cache *cachep)
+{
+	struct corrupter_obj *obj0, *obj1, *obj2;
+	obj0 = obj1 = obj2 = NULL;
+
+	STEP_B1;
+	STEP_B2;
+	STEP_B3a;
+}
